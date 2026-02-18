2f37bf699d0a2c6e9f4bc502a687edcaa5378fce
BERZANWORKWEAR <berzan@berzan.com.tr>
2026-02-18 11:35:02 +0300

    Fix Vite entries for clean folder URLs

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ const pages = [
   'magaza/index.html',
   'urun/index.html',
   'urunler/index.html',
-  'uzman/index.html',
   'iletisim/index.html',
   'tesekkurler/index.html',
   'kvkk/index.html',
   'cerez-politikasi/index.html',
+  'uzman/index.html',
 ];
 
 export default defineConfig({
@@ -21,7 +21,12 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
-      input: Object.fromEntries(pages.map(p => [p.replace('.html',''), resolve(__dirname, p)])),
+      input: Object.fromEntries(
+  pages.map((p) => [
+    p === 'index.html' ? 'index' : p.replace('/index.html', ''),
+    resolve(__dirname, p)
+  ])
+)
       output: {
         entryFileNames: 'assets/js/[name]-[hash].js',
         chunkFileNames: 'assets/js/[name]-[hash].js',
